867f8a10269ffeee9f9f6f0ecb35da7751f4a153
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2024-06-25 18:39:45 -0300

    Corrigindo import não utilizados

--- javaBD/src/javabanco/ConnectionFactory.java
+++ javaBD/src/javabanco/ConnectionFactory.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost/test","root","");
+        return DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
     }
 }
--- javaBD/src/services/RegrasDao.java
+++ javaBD/src/services/RegrasDao.java
@@ -1,16 +1,19 @@
 package services;
 
-import model.Contato;
-
 import java.sql.SQLException;
 import java.util.List;
 
 public interface RegrasDao<T> {
 
     void adiciona(T t) throws SQLException;
+
     List<T> getLista() throws SQLException;
+
     List<T> getListaById(int id) throws SQLException;
-    List<T> getListaByInicial (char inicial) throws SQLException;
+
+    List<T> getListaByInicial(char inicial) throws SQLException;
+
     void updateById(int id, T t) throws SQLException;
+
     void deleteById(int id, T t) throws SQLException;
 }
